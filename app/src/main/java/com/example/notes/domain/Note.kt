28dfd66cc2d910d013d4bc4d987a14c2e7caccdd
Alexey Ivannikov <ivannikov495@gmail.com>
2023-09-13 23:28:58 +0700

    init commit

---
+++ app/src/main/java/com/example/notes/domain/Note.kt
@@ -0,0 +1,7 @@
+package com.example.notes.domain
+
+data class Note (
+    val id: Int,
+    val title: String,
+    val description: String
+        )
---
+++ app/src/main/java/com/example/notes/domain/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.notes.domain
+
+interface NoteRepository {
+
+    fun getNoteList(): List<Note>
+
+    fun getNote(noteId: Int)
+
+    fun addNote(note: Note)
+
+    fun removeNote(note: Note)
+
+    fun editNote(note: Note)
+}
